ea1b70cb4b44508601fd94ef45ef69d094381c2f
Ichchhitmd <hunterichchhit@live.com>
2024-10-20 16:11:25 +0545

    Add dynamic placeholder and submit button

--- cms/furbio-cms/src/app/pages/product/miniForm.tsx
+++ cms/furbio-cms/src/app/pages/product/miniForm.tsx
@@ -1,93 +1,93 @@
-import React from "react";
+// import React from "react";
 
-export const SizeForm = () => {
-  return (
-    <div className="w-full mx-28 my-28 flex flex-col gap-4">
-      Add Size
-      <div>
-        <input
-          type="text"
-          placeholder="Size Name"
-          className="p-2 border border-gray-300 rounded-lg"
-        />
-      </div>
-      <div>
-        <button
-          type="submit"
-          className="p-2 bg-blue-500 text-white rounded-lg w-"
-        >
-          Submit
-        </button>
-      </div>
-    </div>
-  );
-};
+// export const SizeForm = () => {
+//   return (
+//     <div className="w-full mx-28 my-28 flex flex-col gap-4">
+//       Add Size
+//       <div>
+//         <input
+//           type="text"
+//           placeholder="Size Name"
+//           className="p-2 border border-gray-300 rounded-lg"
+//         />
+//       </div>
+//       <div>
+//         <button
+//           type="submit"
+//           className="p-2 bg-blue-500 text-white rounded-lg w-"
+//         >
+//           Submit
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export const OriginForm = () => {
-  return (
-    <div className="w-full mx-28 my-28 flex flex-col gap-4">
-      Add Origin
-      <div>
-        <input
-          type="text"
-          placeholder="Country Name"
-          className="p-2 border border-gray-300 rounded-lg"
-        />
-      </div>
-      <div>
-        <button
-          type="submit"
-          className="p-2 bg-blue-500 text-white rounded-lg w-"
-        >
-          Submit
-        </button>
-      </div>
-    </div>
-  );
-};
+// export const OriginForm = () => {
+//   return (
+//     <div className="w-full mx-28 my-28 flex flex-col gap-4">
+//       Add Origin
+//       <div>
+//         <input
+//           type="text"
+//           placeholder="Country Name"
+//           className="p-2 border border-gray-300 rounded-lg"
+//         />
+//       </div>
+//       <div>
+//         <button
+//           type="submit"
+//           className="p-2 bg-blue-500 text-white rounded-lg w-"
+//         >
+//           Submit
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export const TagForm = () => {
-  return (
-    <div className="w-full mx-28 my-28 flex flex-col gap-4">
-      Add Tag
-      <div>
-        <input
-          type="text"
-          placeholder="Tag Name"
-          className="p-2 border border-gray-300 rounded-lg"
-        />
-      </div>
-      <div>
-        <button
-          type="submit"
-          className="p-2 bg-blue-500 text-white rounded-lg w-"
-        >
-          Submit
-        </button>
-      </div>
-    </div>
-  );
-};
+// export const TagForm = () => {
+//   return (
+//     <div className="w-full mx-28 my-28 flex flex-col gap-4">
+//       Add Tag
+//       <div>
+//         <input
+//           type="text"
+//           placeholder="Tag Name"
+//           className="p-2 border border-gray-300 rounded-lg"
+//         />
+//       </div>
+//       <div>
+//         <button
+//           type="submit"
+//           className="p-2 bg-blue-500 text-white rounded-lg w-"
+//         >
+//           Submit
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export const ColorForm = () => {
-  return (
-    <div className="w-full mx-28 my-28 flex flex-col gap-4">
-      Add Color
-      <div>
-        <input
-          type="text"
-          placeholder="Color Name"
-          className="p-2 border border-gray-300 rounded-lg"
-        />
-      </div>
-      <div>
-        <button
-          type="submit"
-          className="p-2 bg-blue-500 text-white rounded-lg w-"
-        >
-          Submit
-        </button>
-      </div>
-    </div>
-  );
-};
+// export const ColorForm = () => {
+//   return (
+//     <div className="w-full mx-28 my-28 flex flex-col gap-4">
+//       Add Color
+//       <div>
+//         <input
+//           type="text"
+//           placeholder="Color Name"
+//           className="p-2 border border-gray-300 rounded-lg"
+//         />
+//       </div>
+//       <div>
+//         <button
+//           type="submit"
+//           className="p-2 bg-blue-500 text-white rounded-lg w-"
+//         >
+//           Submit
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
